db33654e69d198647c3f0a0c4f737213cddbfe4c
ioanabd <ioana.bulea@student.upt.ro>
2023-03-11 18:00:09 +0200

    Homepage design changed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Color? fundal = Colors.amber;
   @override
   Widget build(BuildContext context) {
     return SafeArea(child:
@@ -21,40 +22,62 @@ class _HomePageState extends State<HomePage> {
             )
         ),
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 100, 20, 100),
-          child: Column(
-            children: [
-              Text(
-                'Welcome!',
-                style: TextStyle(
-                    color: Colors.lightBlue,
-                    fontSize: 30,
-                    decoration: TextDecoration.none
-                ),
-              ),
-              Text(
-                'Find an accomodation perfect for you by answering some simple questions',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.lightBlue,
-                    fontSize: 30,
-                    decoration: TextDecoration.none
-                ),
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
-                ),
-                onPressed: startQuiz,
-                child: Text(
-                  'Get started',
-                  style: TextStyle(
-                      fontSize: 18,
-                      decoration: TextDecoration.none
+          padding: const EdgeInsets.fromLTRB(0, 350, 0, 100),
+          child: Card(
+            color: Colors.transparent,
+            child: SizedBox(
+              width: 370,
+              height: 80,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0,0,170,0),
+                    child: Text(
+                      'Welcome!',
+                      style: TextStyle(
+                          color: Colors.amber[800],
+                          fontSize: 39,
+                          decoration: TextDecoration.none,
+                          fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(25,10,10,20),
+                    child: Text(
+                      'Find the perfect accommodation for you by answering some simple questions.',
+                      style: TextStyle(
+                          color: Colors.amber[600],
+                          fontSize: 24,
+                          decoration: TextDecoration.none
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 340,
+                    height: 60,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.amber[800],
+                        onPrimary: Colors.black,
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(34.0)),
+                      ),
+                      onPressed: startQuiz,
+                      child: Text(
+                        'Get started',
+                        style: TextStyle(
+                            fontSize: 27,
+                            decoration: TextDecoration.none
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         )
     )

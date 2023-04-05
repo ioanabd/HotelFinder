b53955aa19cd0a4fc1bdd0d4cbc60501c53b2243
ioanabd <ioana.bulea@student.upt.ro>
2023-04-05 22:28:24 +0300

    Code cleanup

--- lib/providers/question_provider.dart
+++ lib/providers/question_provider.dart
@@ -16,14 +16,9 @@ class QuestionProvider extends ChangeNotifier{
 
   Future _getQuestionsFuture() async {
     questions = await _questionService.getQuestions();
-    // questions = [new Question(name: 'facilitati', questionText: 'Cati ani ai?', answers: ['Cati ani ai?1', 'Cati ani ai?2', 'Cati ani ai?3']), new Question(name: 'facilitati_camera', questionText: 'Cati ani ai?', answers: ['asdfg1', 'cvbn2', 'sdfgh3'])];
   }
 
   Future getHotelsFuture(List<Question> answers) async {
     hotels = await _questionService.getHotels(answers);
-    // var hotel = new Hotel(Name: "Hotel Imperial Premium", PropertyType: "Hotel", Capacity: "20", Stars: "4", Breakfast: "da", Facilities: ["Wi-fi", "Parcare", "Recepție non-stop", "Sală fitness", "Saună", "Spa"], RoomFacilities: ["Frigider", "Prosoape", "Cadă", "Tv", "Uscător de păr", "Priveliște"], PrivateBathroom: "da", Rating: "Superb 9+", DistanceToCenter: "Mai puțin de 3 km");
-    // var hotel2 = new Hotel(Name: "Pensiunea Vlad", PropertyType: "Hotel", Capacity: "6", Stars: "3", Breakfast: "nu", Facilities: ["Wi-fi", "Parcare", "Recepție non-stop"], RoomFacilities: ["Balcon", "Bucătărie", "Frigider", "Prosoape", "Cadă", "Tv", "Priveliște"], PrivateBathroom: "da", Rating: "Bine 7+", DistanceToCenter: "Mai puțin de 3 km");
-    // hotels.add(hotel);
-    // hotels.add(hotel2);
   }
 }
